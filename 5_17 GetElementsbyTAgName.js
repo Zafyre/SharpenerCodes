2c136be658dcb2b88e040e75eb3a49c8fdadb75a
Zafyre <37234423+Zafyre@users.noreply.github.com>
2023-05-21 13:06:28 +0530

    Add files via upload

---
+++ 5_17 GetElementsbyTAgName.js
@@ -0,0 +1,7 @@
+var new_lst = document.getElementsByClassName("new_list_item");
+console.log(new_lst);
+new_lst[0].style.color = "red";
+
+var all_list = document.getElementsByTagName("li")[4];
+console.log(all_list);
+all_list.innerText="msg";
