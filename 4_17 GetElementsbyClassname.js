81c8dfa194eed1abdf33090f0e0114527d24d9fe
Zafyre <37234423+Zafyre@users.noreply.github.com>
2023-05-21 13:05:26 +0530

    Add files via upload

---
+++ 4_17 GetElementsbyClassname.js
@@ -0,0 +1,5 @@
+var third = document.getElementsByClassName("list-group-item");
+third[2].style.backgroundColor="Green";
+for(var i = 0; i < third.length;i++){
+    third[i].style.fontWeight = "bold";
+}
