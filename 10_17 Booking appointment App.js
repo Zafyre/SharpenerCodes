cce9c15a07efbea725bf4746f3a791d577035656
Zafyre <37234423+Zafyre@users.noreply.github.com>
2023-05-21 13:13:58 +0530

    Add files via upload

---
+++ 10_17 Booking appointment App.js
@@ -0,0 +1,50 @@
+//used for showing msg
+//console.error uesd to show error and console.warn for warning
+console.log(5);
+
+//template string or template literals
+let name = "Aakash";
+console.log(`Hii my name is ${name}`);
+
+
+//to convert array-object code to json file first we convert it so we have method stringify
+//sample example
+var jsonName = JSON.stringify(name);
+
+
+//high order arrays 
+//it contains forEach map filter
+
+
+
+//new Date() methods convert written date to computer date
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////to do 
+//prototype 
